7f0ea0065dfee87f1f09782d462a65c39ca5328a
Varun0kumar <005varunkumar@email.com>
2026-04-29 00:03:47 +0530

    Fixed for Mobile Screens

--- js/main.js
+++ js/main.js
@@ -140,3 +140,15 @@ function openTabAndScroll(event, tabName) {
     }
   }, 500); // slightly smoother timing
 }
+
+document.querySelectorAll('.card-light').forEach(card => {
+  card.addEventListener('mousemove', (e) => {
+    const rect = card.getBoundingClientRect();
+
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    card.style.setProperty('--x', `${x}px`);
+    card.style.setProperty('--y', `${y}px`);
+  });
+});
